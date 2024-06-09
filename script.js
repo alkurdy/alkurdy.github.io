ca65054cca0ed471e255b8c1c7e81bb4428405b0
Aleksey Kurdyukov <alkurdy@gmail.com>
2024-06-08 22:38:50 -0400

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const birthDate = new Date('1988-09-05');
+    const ageSpan = document.getElementById('age');
+
+    const calculateAge = (birthDate) => {
+        const today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        return age;
+    };
+
+    ageSpan.textContent = calculateAge(birthDate);
+});
